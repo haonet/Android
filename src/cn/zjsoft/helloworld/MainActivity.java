ab652ce3bab58e821662d5b249ee270d7beb7999
haonet <fjinhao@hotmail.com>
2016-03-11 23:31:45 +0800

    the first android application

---
+++ src/cn/zjsoft/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package cn.zjsoft.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        TextView textView = (TextView) super.findViewById(R.id.mytext);
+//        textView.setText(R.string.mytext);
+//        Button but = (Button) super.findViewById(R.id.mybot);
+//        but.setText(R.string.mybut);
+        
+    }
+
+
+  
+    
+}
